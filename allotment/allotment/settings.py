fa9cc664386e5957132ca2933f9367f299d8bf1b
Becky Smith <becky.smith@jisc.ac.uk>
2018-12-09 17:51:32 +0000

    heroku

--- allotment/allotment/settings.py
+++ allotment/allotment/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_heroku'
 ]
 
 MIDDLEWARE = [
@@ -76,12 +77,14 @@ WSGI_APPLICATION = 'allotment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'allotment',
+        'USER': 'allotment',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
-DATABASE_URL = 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators

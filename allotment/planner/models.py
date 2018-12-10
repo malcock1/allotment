5dbedd2445b99fcf6cae806df976b4cacace6c69
Becky Smith <becky.smith@jisc.ac.uk>
2018-12-10 14:02:24 +0000

    Starting off models

---
+++ allotment/planner/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+MONTHS = enumerate(['January',
+					'February',
+					'March',
+					'April',
+					'May',
+					'June',
+					'July',
+					'August',
+					'September',
+					'October',
+					'November',
+					'December',],1)
+
+# Create your models here.
+class Month(models.Model):
+	"""
+	We might want to use this to determine what to work on
+	EG wanna see all jobs for March without searching each possible task
+	(planting, harvest etc) individually
+
+	If we do, let's use the ID as the month number - so January = 1, December = 12
+	We can make sure that happens using fixtures :)
+
+	For now on the Plant I'll just use an Integer field rather than FK to a month
+	"""
+	name = models.CharField(max_length=10)
---
+++ allotment/plants/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+LIGHT_CHOICES = [
+	(1,'Full shade'),
+	(2,'Partial shade'),
+	(3,'Shade tolerant'),
+	(4,'Partial sun'),
+	(5,'Full sun'),
+]
+
+class PlantSpecies(models.Model):
+	name = models.CharField(max_length=128)
+	latin_name = models.CharField(max_length=128, null=True, blank=True)
+
+	seed_indoors = models.ManyToManyField("planner.Month", related_name='plants_to_seed_indoors')
+	seed_outdoors = models.ManyToManyField("planner.Month", related_name='plants_to_seed_outdoors')
+	plant_out = models.ManyToManyField("planner.Month", related_name='plants_to_plant_out')
+	harvest = models.ManyToManyField("planner.Month", related_name='plants_to_harvest')
+
+	full_height = models.PositiveSmallIntegerField(null=True, blank=True) # In centimeters
+	full_diameter = models.PositiveSmallIntegerField(null=True, blank=True) # In centimeters
+	light = models.PositiveSmallIntegerField(choices=LIGHT_CHOICES, null=True, blank=True)
+	water = models.PositiveSmallIntegerField(null=True, blank=True) # How much it needs from 1-5
+
+	notes = models.TextField()
+
+	annual = models.BooleanField(default=False)
+
+	companions = models.ManyToManyField("self", related_name='companions') # Species that go well together
+
+
+class Bed(models.Model):
+	"""
+	Location in the allotment 
+	We might have to do something like the tile system on plantz...
+	This is very very unfinished :P
+	"""
+	x = models.PositiveSmallIntegerField(null=True)
+	y = models.PositiveSmallIntegerField(null=True)
+	
+
+class PlantSource(models.Model):
+	name = models.CharField(max_length=128)
+	web_address = models.CharField(max_length=256)
+
+
+class Plant(models.Model):
+	""" An instance of a real physical plant or seed """
+	species = models.ForeignKey(PlantSpecies, on_delete=models.PROTECT)
+	location = models.ForeignKey(Bed, null=True, on_delete=models.SET_NULL)
+	# Could put harvested weight in here?
+	source = models.ForeignKey(PlantSource, null=True, on_delete=models.SET_NULL)
+	cost = models.PositiveSmallIntegerField() # In pence per plant/seed
+
+
+class Harvest(models.Model):
+	"""
+	When we harvest we can store some info about the plants,
+	quantities, comments etc
+	"""
+	plants = models.ManyToManyField(Plant)
+	# Not sure about this bit, probably put it in too soon
+	# Consider having M2M field for beds, plant species
